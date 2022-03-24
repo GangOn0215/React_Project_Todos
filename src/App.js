874f6f8c6fff0d8a5f67869ad78442f7e6c0c056
GangOn0215 <ekdldktnekf@gmail.com>
2022-03-24 21:41:25 +0900

    axios를 이용하여 json api 받아오는것을 공부하였습니다.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import "./App.css";
-import React, { useState, useRef } from "react";
+import React, {useEffect, useState, useRef} from "react";
+import axios from 'axios';
 import TodosHeader from "./Components/TodosHeader";
 import TodosList from "./Components/TodosList";
 import LifeCycle from "./Study/LifeCycle";
 
 function App() {
   const [data, setData] = useState([]);
-
   const dataId = useRef(0);
+
+  const url = 'https://jsonplaceholder.typicode.com/comments';
+  const getData = async () => {
+    const getFakeJsonData = await axios(url).then((res) => res.data);
+
+    const processedData = getFakeJsonData.slice(0, 19).map((item) => {
+      return {
+        author: item.email,
+        todos: item.body,
+        create_date: new Date().getTime(),
+        id: dataId.current++
+      }
+    });
+    
+    setData(processedData);
+  }
+
+  useEffect(() => {
+    getData();
+  }, [])
 
   const onCreate = (author, todos) => {
     const create_date = new Date().getTime();
@@ -16,12 +36,15 @@ function App() {
       todos,
       create_date,
       check: false,
-      id: dataId.current,
+      id: dataId.current
     };
 
     dataId.current += 1;
 
-    setData([newData, ...data]);
+    setData([
+      newData,
+      ...data
+    ]);
   };
 
   const onRemove = (id) => {
@@ -33,31 +56,31 @@ function App() {
   };
 
   const onEdit = (targetId, newTodo) => {
-    const newTodoLists = data.map((item) =>
-      item.id === targetId ? { ...item, todos: newTodo } : item
-    );
+    const newTodoLists = data.map((item) => item.id === targetId ? {
+      ...item,
+      todos: newTodo
+    } : item);
 
     setData(newTodoLists);
   };
 
   const onCheck = (targetId) => {
-    const newTodoLists = data.map((item) =>
-      item.id === targetId ? { ...item, check: !item.check } : item
-    );
+    const newTodoLists = data.map((item) => item.id === targetId ? {
+      ...item,
+      check: !item.check
+    } : item);
 
     setData(newTodoLists);
   };
 
   return (
     <div className="todos-container">
-      <LifeCycle />
-      <TodosHeader onCreate={onCreate} />
-      <TodosList
-        onCheck={onCheck}
+      <LifeCycle/>
+      <TodosHeader onCreate={onCreate}/>
+      <TodosList onCheck={onCheck}
         onEdit={onEdit}
         onRemove={onRemove}
-        todosList={data}
-      />
+        todosList={data}/>
     </div>
   );
 }
